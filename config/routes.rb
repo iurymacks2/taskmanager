aa5f8bb1288b8c868fea83116877ab5676ba4a8e
iurymacks2 <iury.macks@gmail.com>
2019-03-04 08:06:59 -0300

    keep going 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users do
+  
     resources :todo_lists
-  end
+
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
